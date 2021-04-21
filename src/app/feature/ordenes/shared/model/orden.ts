b090080fcb283d968fb4ae1d8ddc12ca9352250d
juan.munoz <juan.munoz@ceiba.com.co>
2021-04-21 14:05:03 -0500

    Se corrige valor total

--- src/app/feature/ordenes/shared/model/orden.ts
+++ src/app/feature/ordenes/shared/model/orden.ts
@@ -4,15 +4,15 @@ export class Orden{
     private placa:String;
     private fechaIngreso:Date;
     private fechaSalida:Date;
-    private total:number;
+    private valorTotal:number;
 
 
-    constructor(idOrden:number,placa:String,fechaIngreso:Date,fechaSalida:Date,total:number){
+    constructor(idOrden:number,placa:String,fechaIngreso:Date,fechaSalida:Date,valorTotal:number){
         this.idOrden=idOrden;
         this.placa=placa;
         this.fechaIngreso=fechaIngreso;
         this.fechaSalida=fechaSalida;
-        this.total=total;
+        this.valorTotal=valorTotal;
     }
 
 
